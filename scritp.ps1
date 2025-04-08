259c7c235a4f6e2c3465cdf15d44f10055ee35c7
generalniy <alisher@MacBookPro.attlocal.net>
2025-04-08 13:35:08 -0500

    my secnd change

--- scritp.ps1
+++ scritp.ps1
@@ -23,6 +23,17 @@ function sdfsdfsd
 2000000
 
 
+
+
+
 100000
 
+
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/INgeeg/nimet.git
+git push -u origin main
 
